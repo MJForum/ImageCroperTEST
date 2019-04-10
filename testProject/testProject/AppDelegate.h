bdcd5501a403838394cf2af7eae404be0034cfc4
马建 <majian@majiandeiMac-2.local>
2019-04-10 16:08:34 +0800

    test

---
+++ testProject/testProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by 马建 on 2019/4/10.
+//  Copyright © 2019 com.aaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
